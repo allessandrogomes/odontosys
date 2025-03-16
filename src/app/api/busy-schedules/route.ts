78358a900eb4417092577fed71a27d45df2306f4
Alessandro <alllessandrogomes@gmail.com>
2025-03-16 18:49:24 -0300

    :recycle:refactor: melhora o endpoint de busy-schedules.

--- src/app/api/busy-schedules/route.ts
+++ src/app/api/busy-schedules/route.ts
@@ -2,28 +2,29 @@ import prisma from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
 
-// GET /api/busy/schedules
+// GET /api/busy-schedules
 export async function GET() {
     try {
         // Busca todas as Consultas Atuais e Horários Bloqueados
-        const appointments = await prisma.appointment.findMany()
-        const blockedTimes = await prisma.blockedTime.findMany()
+        const [appointments, blockedTimes] = await Promise.all([
+            prisma.appointment.findMany(),
+            prisma.blockedTime.findMany()
+        ])
 
-        // Cria um array com todos os horários ocupados
-        const busySchedules = !appointments ? [] : appointments.map(item => item.scheduledAt)
-
-        // Cria um array com todos os horários bloqueados
-        const arrayOfBlockedTimes = !blockedTimes ? [] : blockedTimes.map(item => item.blockedTime)
+        // Extrai os horários ocupados e bloqueados
+        const busySchedules = appointments.map(item => item.scheduledAt)
+        const arrayOfBlockedTimes = blockedTimes.map(item => item.blockedTime)
 
         // Concatena todos os horários ocupados/bloqueados
-        const allBlockedTimes = [...(busySchedules || []), ...(arrayOfBlockedTimes || [])]
+        const allBlockedTimes = [...busySchedules, ...arrayOfBlockedTimes]
 
         return NextResponse.json(allBlockedTimes, { status: 200 })
+
     } catch (error) {
         // Tratamento de erros
-        console.error(error)
+        console.error("Erro ao buscar horários ocupados/bloqueados:", error)
         return NextResponse.json(
-            { error: "Erro no servidor", details: error },
+            { error: "Erro interno no servidor", details: String(error) },
             { status: 500 }
         )
     }
