88a5796ac9c41e384c7830bc24b804fbcca76008
Alessandro <alllessandrogomes@gmail.com>
2025-04-12 23:16:48 -0300

    :sparkles:feat: adiciona a funcionalidade de login no front end.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,48 @@
+'use client'
 
+import { useState } from "react"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    login: "",
+    password: ""
+  })
+
+  const router = useRouter()
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    const response = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        cpf: formData.login,
+        password: formData.password
+      })
+    })
+
+    const data = await response.json()
+
+    if (!response.ok) {
+      alert(data.error || "Erro ao fazer o login")
+    } else {
+      router.push("/dentist-dashboard")
+    }
+  }
+
   return (
-    <>Em desenvolvimento</>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>Login</label>
+        <input type="text"  onChange={e => setFormData({ ...formData, login: e.target.value })} value={formData.login}/>
+        <label>Senha</label>
+        <input type="password" onChange={e => setFormData({ ...formData, password: e.target.value })} value={formData.password}/>
+        <button type="submit">Entrar</button>
+      </form>
+    </>
   )
 }
