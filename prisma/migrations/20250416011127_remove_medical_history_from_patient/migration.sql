52c729734017b276e030283b368708c8347316c6
Alessandro <alllessandrogomes@gmail.com>
2025-04-15 22:30:51 -0300

    :wastebasket:remove: deleta propriedade 'medicalHistory' de 'patient' no prisma schema.

---
+++ prisma/migrations/20250416011127_remove_medical_history_from_patient/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `medicalHistory` on the `Patient` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Patient" DROP COLUMN "medicalHistory";
