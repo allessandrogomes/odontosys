3d9b58ea8c53093be19f3b718da3f618e9b936f4
Alessandro <alllessandrogomes@gmail.com>
2025-04-28 10:38:42 -0300

    implementa dropdown list no sidebar.

--- src/components/DashboardSideBar/index.tsx
+++ src/components/DashboardSideBar/index.tsx
@@ -3,20 +3,45 @@ import { IoMdHome } from "react-icons/io"
 import { FaCalendarAlt } from "react-icons/fa"
 import { FaRegUser } from "react-icons/fa"
 import { TbReportAnalytics } from "react-icons/tb"
+import { useState } from "react"
 
 const sidebarBtns = [
-    { icon: <IoMdHome className={styles.icon}/>, title: "Início" },
-    { icon: <FaCalendarAlt className={styles.icon} />, title: "Agendamentos" },
-    { icon: <FaRegUser className={styles.icon}/>, title: "Pacientes" },
-    { icon: <TbReportAnalytics className={styles.icon}/>, title: "Relatórios" }
+    { icon: <IoMdHome className={styles.icon} />, title: "Início" },
+    { icon: <FaCalendarAlt className={styles.icon} />, title: "Agendamentos", childrens: ["Nova Consulta", "Reagendar Consulta", "Cancelar Consulta", "Alterar Consulta"] },
+    { icon: <FaRegUser className={styles.icon} />, title: "Pacientes", childrens: ["Pesquisar", "Cadastrar", "Editar informações"] },
+    { icon: <TbReportAnalytics className={styles.icon} />, title: "Relatórios" }
 ]
 
 export default function DashboardSideBar() {
+    const [openMenu, setOpenMenu] = useState<string | null>(null)
+
+    function toggleMenu(title: string) {
+        setOpenMenu(prev => (prev === title ? null : title))
+      }
+
     return (
         <aside className={styles.aside}>
             <nav>
                 <ul>
-                    {sidebarBtns.map(btn => <li key={btn.title}>{btn.icon} <h3>{btn.title}</h3></li>)}
+                    {sidebarBtns.map(btn => (
+                        <li key={btn.title} className={styles.menu}>
+                            <div className={styles.menuItem} onClick={() => btn.childrens && toggleMenu(btn.title)}>
+                                {btn.icon}
+                                <h3>{btn.title}</h3>
+                            </div>
+
+                            {/* Se tiver filhos e o menu estiver aberto */}
+                            {btn.childrens && openMenu === btn.title && (
+                                <ul className={styles.submenu}>
+                                    {btn.childrens.map((child, index) => (
+                                        <li key={index} className={styles.submenuItem}>
+                                            {child}
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </li>
+                    ))}
                 </ul>
             </nav>
         </aside>
