86b72f6a55eecee8f677d1b85000f1b675068d93
Alessandro <alllessandrogomes@gmail.com>
2025-05-06 12:08:07 -0300

    :bug:fix: define o 'timeZone' da função 'formatHour' para São Paulo.

--- src/utils/formatHour.ts
+++ src/utils/formatHour.ts
@@ -2,7 +2,8 @@ export function formatHour(hourISO: string) {
     if (!hourISO) return ""
     const hour = new Date(hourISO).toLocaleTimeString("pt-BR", {
         hour: "2-digit",
-        minute: "2-digit"
+        minute: "2-digit",
+        timeZone: "America/Sao_Paulo"
     })
 
     return hour
