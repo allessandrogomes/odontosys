44cbe8d11d5e7f0e1e28358567b4f928695e3290
Alessandro <alllessandrogomes@gmail.com>
2025-07-18 10:30:55 -0300

    :wrench:build: atualiza script que gera última atualização.

--- scripts/update-timestamp.mjs
+++ scripts/update-timestamp.mjs
@@ -1,7 +1,5 @@
 import { writeFileSync } from "fs"
 
-// Script para atualizar a data da última atualização
-
 const timestamp = new Date().toISOString()
 const content = `// Gerado automaticamente\nexport const lastUpdate = '${timestamp}'\n`
 
