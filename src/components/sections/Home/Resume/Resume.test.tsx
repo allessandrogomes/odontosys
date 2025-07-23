8ca57bb75811eea3da5ec76b3d46024805f06265
Alessandro <alllessandrogomes@gmail.com>
2025-07-23 12:01:58 -0300

    :test_tube:test: verifica a existência dos títulos 'Dentista' e 'Consultas' e o botão 'Atualizar'.

---
+++ src/components/sections/Home/Resume/Resume.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen, waitFor } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import Resume from "."
+
+// Mock global.fetch para evitar chamadas reais no useEffect
+
+beforeAll(() => {
+    global.fetch = jest.fn(() =>
+        Promise.resolve({
+            ok: true,
+            json: () => Promise.resolve([]) // Retorna um array vazio de appointments
+        })
+    ) as jest.Mock
+})
+
+describe("Dashboard do Recepcionista - Resume", () => {
+    // Testes de renderização
+    it("deve exibir os títulos 'Dentista' e 'Consultas' e o botão 'Atualizar'", async () => {
+        render(<Resume />)
+
+        await waitFor(() => {
+            expect(screen.getByRole("heading", { name: /dentista/i })).toBeInTheDocument()
+            expect(screen.getByRole("heading", { name: /consultas/i })).toBeInTheDocument()
+            expect(screen.getByRole("button", { name: /atualizar/i })).toBeInTheDocument()
+        })
+    })
+})
