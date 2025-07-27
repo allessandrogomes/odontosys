24f2ad33d2ae517e8920189df3f235645280aca8
Alessandro <alllessandrogomes@gmail.com>
2025-07-27 20:51:56 -0300

    :test_tube:test: testa se mostra corretamente os horários formatados.

--- src/components/sections/Home/Resume/AppointmentCard/AppointmentCard.test.tsx
+++ src/components/sections/Home/Resume/AppointmentCard/AppointmentCard.test.tsx
@@ -7,13 +7,13 @@ describe("AppointmentCard", () => {
     // Testes de renderização
     it("renderiza apenas o primeiro e último nome do paciente", () => {
         render(
-            <AppointmentCard 
+            <AppointmentCard
                 patientName="João da Silva Costa"
                 procedure="Limpeza"
                 start="2025-07-26T14:00:00.000Z"
                 end="2025-07-26T15:00:00.000Z"
                 onClickFinish={jest.fn()}
-                onClickCancel={jest.fn()}      
+                onClickCancel={jest.fn()}
             />
         )
 
@@ -25,16 +25,31 @@ describe("AppointmentCard", () => {
 
     it("exibe corretamente o nome do procedimento", () => {
         render(
-            <AppointmentCard 
+            <AppointmentCard
                 patientName="João da Silva Costa"
                 procedure="Limpeza"
                 start="2025-07-26T14:00:00.000Z"
                 end="2025-07-26T15:00:00.000Z"
                 onClickFinish={jest.fn()}
-                onClickCancel={jest.fn()}      
+                onClickCancel={jest.fn()}
             />
         )
 
         expect(screen.getByText(/Limpeza/i)).toBeInTheDocument()
+    })
+
+    it("mostra corretamente os horários formatados", () => {
+        render(
+            <AppointmentCard
+                patientName="João da Silva Costa"
+                procedure="Limpeza"
+                start="2025-07-26T14:00:00.000Z"
+                end="2025-07-26T15:00:00.000Z"
+                onClickFinish={jest.fn()}
+                onClickCancel={jest.fn()}
+            />
+        )
+
+        expect(screen.getByText(/11:00 - 12:00/i)).toBeInTheDocument()
     })
 })
