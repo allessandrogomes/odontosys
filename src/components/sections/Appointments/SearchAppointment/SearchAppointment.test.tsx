13d959340e66e90d8926774e1aec6e0e825c358e
Alessandro <alllessandrogomes@gmail.com>
2025-07-29 11:05:50 -0300

    :test_tube:test: testa se o componente AppointmentsFound aparece ao receber os dados.

--- src/components/sections/Appointments/SearchAppointment/SearchAppointment.test.tsx
+++ src/components/sections/Appointments/SearchAppointment/SearchAppointment.test.tsx
@@ -1,12 +1,30 @@
-import { render, screen } from "@testing-library/react"
+import { act, render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import SearchAppointment from "."
+import userEvent from "@testing-library/user-event"
 
 // Mock do componente SearchField
 jest.mock("@/components/sections/Appointments/SearchAppointment/SearchField", () => {
-    const MockSearchField = () => <div data-testid="search-field">SearchField</div>
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const MockSearchField = (props: { appointmentsFound: (a: any[]) => void }) => {
+        return (
+            <button
+                data-testid="search-field"
+                onClick={() => props.appointmentsFound([{ id: 1, patientName: "Teste" }])}
+            >
+                SearchField
+            </button>
+        )
+    }
     MockSearchField.displayName = "MockSearchField"
     return MockSearchField
+})
+
+// Mock do AppointmentsFound
+jest.mock("@/components/sections/Appointments/SearchAppointment/AppointmentsFound", () => {
+    return function MockAppointmentsFound() {
+        return <div data-testid="appointments-found">AppointmentsFound</div>
+    }
 })
 
 describe("SearchAppointment", () => {
@@ -21,5 +39,20 @@ describe("SearchAppointment", () => {
         render(<SearchAppointment />)
         const searchField = screen.getByTestId("search-field")
         expect(searchField).toBeInTheDocument()
+    })
+
+    // Testes de interação
+    it("deve mostrar o componente AppointmentsFound ao receber resultados do SearchField", async () => {
+        render(<SearchAppointment />)
+
+        const button = screen.getByTestId("search-field")
+        // Simula clique para disparar o callback que preenche appointments
+        await act(async () => {
+            await userEvent.click(button)
+        })
+
+        // Verifica se o AppointmentFound aparece
+        const appointmentsFound = screen.getByTestId("appointments-found")
+        expect(appointmentsFound).toBeInTheDocument()
     })
 })
