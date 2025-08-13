eefe7bb93d8ffae6e088dfc7c0302abf53dfd5ba
Alessandro <alllessandrogomes@gmail.com>
2025-08-13 12:11:03 -0300

    :lipstick:feat: ajusta estilização background de NewAppointment.

--- src/app/(private)/receptionist-dashboard/Appointments/NewAppointment/index.tsx
+++ src/app/(private)/receptionist-dashboard/Appointments/NewAppointment/index.tsx
@@ -5,17 +5,20 @@ import SelectProcedure from "./SelectProcedure"
 import SelectDentist from "./SelectDentist"
 import SelectScheduled from "./SelectScheduled"
 import ConfirmAppointment from "./ConfirmAppointment"
+import SectionWrapper from "@/components/layout/SectionWrapper"
 
 export default function NewAppointment() {
     const { state } = useAppointmentContext()
     return (
-        <div>
-            {state.step === 1 && <SearchPatient />}
-            {state.step === 2 && <ConfirmPatient />}
-            {state.step === 3 && <SelectProcedure />}
-            {state.step === 4 && <SelectDentist />}
-            {state.step === 5 && <SelectScheduled />}
-            {state.step === 6 && <ConfirmAppointment />}
-        </div>
+        <SectionWrapper title="Nova Consulta">
+            <>
+                {state.step === 1 && <SearchPatient />}
+                {state.step === 2 && <ConfirmPatient />}
+                {state.step === 3 && <SelectProcedure />}
+                {state.step === 4 && <SelectDentist />}
+                {state.step === 5 && <SelectScheduled />}
+                {state.step === 6 && <ConfirmAppointment />}
+            </>
+        </SectionWrapper>
     )
 }
