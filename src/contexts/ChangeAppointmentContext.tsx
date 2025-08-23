c1c135dc42a45ab9db0e044bc63226fdeccf384f
Alessandro <alllessandrogomes@gmail.com>
2025-08-22 22:37:43 -0300

    :recycle:refactor: ajusta o tipo de 'selectedAppointment' para o fluxo de alteração da consulta.

--- src/contexts/ChangeAppointmentContext.tsx
+++ src/contexts/ChangeAppointmentContext.tsx
@@ -1,8 +1,27 @@
 import { createContext, useReducer, useContext, ReactNode } from "react"
 
+// É necessário algumas propriedades terem a possibilidade de serem nulas
+// pois em momentos do fluxo de alteração é necessário resetar valores
+// Exemplo: Ao alterar o procedimento, é preciso resetar o dentista para escolher novamente
+// pois o dentista inicialmente definido pode não atender à aquele procedimento
+// O mesmo vale para dia e horário
+type ModifiableAppointment = {
+    id: number
+    patient: IPatient
+    patientId: number
+    dentist: IDentist | null
+    dentistId: number | null
+    scheduledAt: string | null
+    endsAt: string | null
+    durationMinutes: number | null
+    procedure: string | null
+    status: EAppointmentStatus
+    createdAt: string
+}
+
 type ChangeAppointmentState = {
     appointments: IAppointment[] | [] | null
-    selectedAppointment: IAppointment | null
+    selectedAppointment: ModifiableAppointment | null
     step: number
 }
 
@@ -10,7 +29,7 @@ type State = ChangeAppointmentState
 
 type Action =
     | { type: "SET_APPOINTMENTS"; payload: IAppointment[] | [] | null }
-    | { type: "SET_SELECTED_APPOINTMENT"; payload: IAppointment | null }
+    | { type: "SET_SELECTED_APPOINTMENT"; payload: ModifiableAppointment | null }
     | { type: "SET_STEP"; payload: number }
 
 const initialState: State = {
