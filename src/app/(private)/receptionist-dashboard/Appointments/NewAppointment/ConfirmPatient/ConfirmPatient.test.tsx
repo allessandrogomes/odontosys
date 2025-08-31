d2d1c3833842561982f3fc9e577dbf8fdae6cc3f
Alessandro <alllessandrogomes@gmail.com>
2025-08-31 19:17:22 -0300

    :test_tube:test: valida dispatch dos botões Voltar e Próximo em ConfirmPatient

--- src/app/(private)/receptionist-dashboard/Appointments/NewAppointment/ConfirmPatient/ConfirmPatient.test.tsx
+++ src/app/(private)/receptionist-dashboard/Appointments/NewAppointment/ConfirmPatient/ConfirmPatient.test.tsx
@@ -1,5 +1,5 @@
 import { useAppointmentContext } from "@/contexts/NewAppointmentContext"
-import { render } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import ConfirmPatient from "."
 
 
@@ -21,5 +21,22 @@ describe("ConfirmPatient", () => {
     it("deve renderizar corretamente mostrando o nome do paciente (snapshot)", () => {
         const { container } = render(<ConfirmPatient />)
         expect(container).toMatchSnapshot()
+    })
+
+    it("deve disparar dispatch correto ao clicar em 'Voltar'", () => {
+        render(<ConfirmPatient />)
+        const backButton = screen.getByText("Voltar")
+        fireEvent.click(backButton)
+
+        expect(dispatchMock).toHaveBeenCalledWith({ type: "SET_STEP", payload: 1 })
+        expect(dispatchMock).toHaveBeenCalledWith({ type: "SET_PATIENT", payload: { id: null, name: "" } })
+    })
+
+    it("deve disparar dispatch correto ao clicar em 'Próximo'", () => {
+        render(<ConfirmPatient />)
+        const nextButton = screen.getByText("Próximo")
+        fireEvent.click(nextButton)
+
+        expect(dispatchMock).toHaveBeenCalledWith({ type: "SET_STEP", payload: 3 })
     })
 })
