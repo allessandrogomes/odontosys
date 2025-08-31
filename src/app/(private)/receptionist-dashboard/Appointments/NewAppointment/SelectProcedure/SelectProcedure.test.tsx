cf26f65a601284bb52101c284c837049e5ca812e
Alessandro <alllessandrogomes@gmail.com>
2025-08-31 19:47:43 -0300

    :test_tube:test: cria snapshot inicial de SelectProcedure com procedimentos disponíveis

---
+++ src/app/(private)/receptionist-dashboard/Appointments/NewAppointment/SelectProcedure/SelectProcedure.test.tsx
@@ -0,0 +1,47 @@
+import { useAppointmentContext } from "@/contexts/NewAppointmentContext"
+import useSWR from "swr"
+import SelectProcedure from "."
+import { render, screen } from "@testing-library/react"
+
+// Mock do contexto
+jest.mock("@/contexts/NewAppointmentContext", () => ({
+    useAppointmentContext: jest.fn()
+}))
+
+// Mock do useSWR
+jest.mock("swr")
+
+describe("SelectProcedure", () => {
+    const dispatchMock = jest.fn()
+    const proceduresMock = [
+        { id: 1, procedure: "Limpeza", durationMinutes: 30 },
+        { id: 2, procedure: "Restauração", durationMinutes: 60 }
+    ]
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        (useAppointmentContext as jest.Mock).mockReturnValue({
+            state: { procedure: "" },
+            dispatch: dispatchMock
+        });
+
+        (useSWR as jest.Mock).mockReturnValue({
+            data: proceduresMock,
+            error: null,
+            isLoading: false
+        })
+    })
+
+    it("deve renderizar corretamente e mostrar os procedimentos no select (snapshot)", () => {
+        const { container } = render(<SelectProcedure />)
+
+        const select = screen.getByLabelText("Escolha o Procedimento") as HTMLSelectElement
+        expect(select).toBeInTheDocument()
+        expect(select.value).toBe("") // valor inicial
+        expect(screen.getByText("Selecione um procedimento")).toBeInTheDocument()
+        expect(screen.getByText("Limpeza")).toBeInTheDocument()
+        expect(screen.getByText("Restauração")).toBeInTheDocument()
+
+        expect(container).toMatchSnapshot()
+    })
+})
