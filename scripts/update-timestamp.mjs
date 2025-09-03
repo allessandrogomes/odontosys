9d896711951aa011bffc622e859fe3bfd6c99548
Alessandro <alllessandrogomes@gmail.com>
2025-09-02 22:37:51 -0300

    :wastebasket:remove: deleta arquivos script/action não mais utilizados

--- scripts/update-timestamp.mjs
+++
@@ -1,6 +0,0 @@
-import { writeFileSync } from "fs"
-
-const timestamp = new Date().toISOString()
-const content = `// Gerado automaticamente\nexport const lastUpdate = '${timestamp}'\n`
-
-writeFileSync("src/lastUpdate.ts", content)
--- src/lastUpdate.ts
+++
@@ -1,2 +0,0 @@
-// Gerado automaticamente
-export const lastUpdate = '2025-08-29T14:23:26.333Z'
